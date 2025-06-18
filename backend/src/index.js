91f78eebb0da9e2a36f7a4371c351aeba1094783
Afthar17 <aftharafthar5@gmail.com>
2025-06-18 22:33:31 +0530

    build and wildcard route fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Match any route not starting with /api
 
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
     // Skip API routes
     if (req.originalUrl.startsWith("/api")) return next();
 
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     console.log(__dirname);
   });
 }
